2a3063c8830219ea3d2ff9576c3324cffc4d7549
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2021-08-07 14:00:37 -0400

    Added bytes decoding functions.

--- src/binio/core.clj
+++ src/binio/core.clj
@@ -18,3 +18,67 @@
    (throw (IOException. "Error. Must supply a byte-array to write"))))
 
 
+(defn decode-ubyte
+  [b]
+  (let [b (short b)]
+    (if (neg? b)
+      (+ b 256)
+      b)))
+
+(defn decode-ushort
+  [b0 b1]
+  (int (bit-or (bit-shift-left (decode-ubyte b0) 8) (decode-ubyte b1))))
+
+(defn decode-ulong
+  [b0 b1 b2 b3]
+  (long (bit-or (bit-shift-left (decode-ubyte b0) 24)
+                (bit-shift-left (decode-ubyte b1) 16)
+                (bit-shift-left (decode-ubyte b2) 8)
+                (decode-ubyte b3))))
+
+(defn decode-slong
+  [b0 b1 b2 b3]
+  (int (bit-or (bit-shift-left b0 24)
+               (bit-shift-left b1 16)
+               (bit-shift-left b2 8)
+               b3)))
+
+(defn decode-ulongrational
+  [b0 b1 b2 b3 b4 b5 b6 b7]
+  (/ (decode-ulong b0 b1 b2 b3)
+     (decode-ulong b4 b5 b6 b7)))
+
+(defn decode-slongrational
+  [b0 b1 b2 b3 b4 b5 b6 b7]
+  (/ (decode-slong b0 b1 b2 b3)
+     (decode-slong b4 b5 b6 b7)))
+
+(defn hex [n]
+  (format "0x%02X" n))
+
+(defn dec->bin
+  [n]
+  (->> (repeat n)
+       (map bit-and [128 64 32 16 8 4 2 1])
+       (map #(if (pos? %) 1 0))))
+
+(defn bin->dec
+  [bin-seq]
+  (reduce + (map * bin-seq (reverse (take 8 (iterate (partial * 2) 1))))))
+
+(defn decode-float32
+  [b0 b1 b2 b3]
+  (let [m_exp (take 24 (iterate #(float (/ % 2)) 1))
+        b0 (dec->bin (decode-ubyte b0))
+        b1 (dec->bin (decode-ubyte b1))
+        b2 (dec->bin (decode-ubyte b2))
+        b3 (dec->bin (decode-ubyte b3))
+        bits (concat b0 b1 b2 b3)
+        sign (first bits)
+        exponent (- (bin->dec (take 8 (rest bits))) 127)
+        mantissa (cons 1 (drop 9 bits))]
+     (* (reduce * (repeat sign -1))
+        (reduce + (map * mantissa m_exp))
+        (if (neg? exponent)
+          (reduce / 1 (repeat (- exponent) 2N))
+          (reduce * (repeat exponent 2N))))))
