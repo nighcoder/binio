0ab47bb646eec939f542512cff535d355f04719c
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2021-08-11 15:36:16 -0400

    Changed most decoders' signatures to work sequences of bytes, instead of individual bytes.

--- src/binio/core.clj
+++ src/binio/core.clj
@@ -37,43 +37,43 @@
       b)))
 
 (defn decode-ushort
-  [b0 b1]
+  [[b0 b1]]
   (int (bit-or (bit-shift-left (decode-ubyte b0) 8) (decode-ubyte b1))))
 
 (defn decode-sshort
-  [b0 b1]
+  [[b0 b1]]
   (if (neg? b0)
-    (- (inc (decode-ushort (bit-not b0) (bit-not b1))))
-    (decode-ushort b0 b1)))
+    (- (inc (decode-ushort [(bit-not b0) (bit-not b1)])))
+    (decode-ushort [b0 b1])))
 
 (defn decode-ulong
-  [b0 b1 b2 b3]
+  [[b0 b1 b2 b3]]
   (long (bit-or (bit-shift-left (decode-ubyte b0) 24)
                 (bit-shift-left (decode-ubyte b1) 16)
                 (bit-shift-left (decode-ubyte b2) 8)
                 (decode-ubyte b3))))
 
 (defn decode-slong
-  [b0 b1 b2 b3]
+  [[b0 b1 b2 b3]]
   (if (neg? b0)
-    (- (inc (decode-ulong (bit-not b0) (bit-not b1) (bit-not b2) (bit-not b3))))
-    (decode-ulong b0 b1 b2 b3)))
+    (- (inc (decode-ulong (map bit-not [b0 b1 b2 b3]))))
+    (decode-ulong [b0 b1 b2 b3])))
 
 (defn decode-ulongrational
-  [b0 b1 b2 b3 b4 b5 b6 b7]
-  (/ (decode-ulong b0 b1 b2 b3)
-     (decode-ulong b4 b5 b6 b7)))
+  [[b0 b1 b2 b3 b4 b5 b6 b7]]
+  (/ (decode-ulong [b0 b1 b2 b3])
+     (decode-ulong [b4 b5 b6 b7])))
 
 (defn decode-slongrational
-  [b0 b1 b2 b3 b4 b5 b6 b7]
-  (/ (decode-slong b0 b1 b2 b3)
-     (decode-slong b4 b5 b6 b7)))
+  [[b0 b1 b2 b3 b4 b5 b6 b7]]
+  (/ (decode-slong [b0 b1 b2 b3])
+     (decode-slong [b4 b5 b6 b7])))
 
 (defn hex [n]
   (format "0x%02X" n))
 
 (defn decode-float32
-  [b0 b1 b2 b3]
+  [[b0 b1 b2 b3]]
   (let [m_exp (take 24 (iterate #(float (/ % 2)) 1))
         b0 (encode-bits (decode-ubyte b0))
         b1 (encode-bits (decode-ubyte b1))
--- test/binio/core_test.clj
+++ test/binio/core_test.clj
@@ -30,91 +30,68 @@
 
 (defspec ushort-sign-test 500
   (do (println "Ushort test: The decoded number is an integer between 0 and 65.535 (inclusive)")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte]
-      (and (integer? (decode-ushort b0 b1))
-           (<= 0 (decode-ushort b0 b1) 65535)))))
+    (prop/for-all [b (gen/vector gen/byte 2)]
+      (and (integer? (decode-ushort b))
+           (<= 0 (decode-ushort b) 65535)))))
 
 (defspec ushort-sshort-conversion-test 500
   (do (println "Ushort Sshort conversion test: The decoded sshort converted to a ushort is the same as the decoded ushort")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte]
+    (prop/for-all [[b0 b1 :as b] (gen/vector gen/byte 2)]
       (if (neg? b0)
-        (= (+ 65536 (decode-sshort b0 b1)) (decode-ushort b0 b1))
-        (= (decode-ushort b0 b1) (decode-sshort b0 b1))))))
+        (= (+ 65536 (decode-sshort b)) (decode-ushort b))
+        (= (decode-ushort b) (decode-sshort b))))))
 
 (defspec ulong-test 500
   (do (println "Ulong test: The decoded number is an integer between 0 and 4.294.967.295")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte]
-      (and (integer? (decode-ulong b0 b1 b2 b3))
-           (<= 0 (decode-ulong b0 b1 b2 b3) 4294967295)))))
+    (prop/for-all [b (gen/vector gen/byte 4)]
+      (and (integer? (decode-ulong b))
+           (<= 0 (decode-ulong b) 4294967295)))))
 
 (defspec ulong-sslong-conversion-test 500
   (do (println "Ulong Slong conversion test: The decoded slong converted to a ulong is the same as the decoded ulong")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte]
+    (prop/for-all [[b0 & _ :as b] (gen/vector gen/byte 4)]
       (if (neg? b0)
-        (= (+ 4294967296 (decode-slong b0 b1 b2 b3)) (decode-ulong b0 b1 b2 b3))
-        (= (decode-ulong b0 b1 b2 b3) (decode-slong b0 b1 b2 b3))))))
+        (= (+ 4294967296 (decode-slong b)) (decode-ulong b))
+        (= (decode-ulong b) (decode-slong b))))))
 
 (defspec ulong-rational-pos-test 500
   (do (println "Ulong rational positive test: The decoded number is a positive rational")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte
-                   [b4 b5 b6 b7] (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
-      (let [res (decode-ulongrational b0 b1 b2 b3 b4 b5 b6 b7)]
+    (prop/for-all [b0 (gen/vector gen/byte 4)
+                   b1 (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
+      (let [res (decode-ulongrational (concat b0 b1))]
         (and (pos? res) (rational? res))))))
 
 (defspec ulong-rational-unit-test 500
   (do (println "Ulong rational unit test: The decoded number is > 1 when numerator > denominator")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte
-                   [b4 b5 b6 b7] (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
-      (let [res (decode-ulongrational b0 b1 b2 b3 b4 b5 b6 b7)]
-        (if (> (decode-ulong b0 b1 b2 b3) (decode-ulong b4 b5 b6 b7))
+    (prop/for-all [b0 (gen/vector gen/byte 4)
+                   b1 (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
+      (let [res (decode-ulongrational (concat b0 b1))]
+        (if (> (decode-ulong b0) (decode-ulong b1))
           (> res 1)
           (<= res 1))))))
 
 (defspec slong-rational-test 500
   (do (println "Slong rational test: The decoded number is rational")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte
-                   [b4 b5 b6 b7] (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
-      (let [res (decode-ulongrational b0 b1 b2 b3 b4 b5 b6 b7)]
+    (prop/for-all [b0 (gen/vector gen/byte 4)
+                   b1 (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
+      (let [res (decode-ulongrational (concat b0 b1))]
         (rational? res)))))
 
 (defspec slong-rational-sign-test 500
   (do (println "Slong rational sign test: The decoded number is positive if both the numerator and denominator are either positive or negative")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte
-                   [b4 b5 b6 b7] (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
-      (let [res (decode-slongrational b0 b1 b2 b3 b4 b5 b6 b7)]
-        (if (or (and (pos? (decode-slong b0 b1 b2 b3)) (pos? (decode-slong b4 b5 b6 b7)))
-                (and (neg? (decode-slong b0 b1 b2 b3)) (neg? (decode-slong b4 b5 b6 b7))))
+    (prop/for-all [b0 (gen/vector gen/byte 4)
+                   b1 (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
+      (let [res (decode-slongrational (concat b0 b1))]
+        (if (or (and (pos? (decode-slong b0)) (pos? (decode-slong b1)))
+                (and (neg? (decode-slong b0)) (neg? (decode-slong b1))))
           (pos? res)
           (neg? res))))))
 
 (defspec slong-rational-unit-test 500
   (do (println "Slong rational unit test: The decoded number in absolute value is > 1 when abs(numerator) > abs(denominator)")
-    (prop/for-all [b0 gen/byte
-                   b1 gen/byte
-                   b2 gen/byte
-                   b3 gen/byte
-                   [b4 b5 b6 b7] (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
-      (let [res (decode-slongrational b0 b1 b2 b3 b4 b5 b6 b7)]
-        (if (> (abs (decode-slong b0 b1 b2 b3)) (abs (decode-slong b4 b5 b6 b7)))
+    (prop/for-all [b0 (gen/vector gen/byte 4)
+                   b1 (gen/such-that #(not= 0 (reduce + %)) (gen/vector gen/byte 4))]
+      (let [res (decode-slongrational (concat b0 b1))]
+        (if (> (abs (decode-slong b0)) (abs (decode-slong b1)))
           (> (abs res) 1)
           (<= (abs res) 1))))))
