7707b8297a2ebdb8e620bc7a663722390fe7390d
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2021-08-23 08:25:31 -0400

    Added entry to changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,17 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## [0.1.1] - 2021-08-23
+- Added decoding functions for various data types:
+  - unsigned byte
+  - signed short (16 bits)
+  - unsigned short (16 bits)
+  - signed long (32 bits)
+  - unsigned long (32 bits)
+  - signed long rational (64 bits)
+  - unsigned long rational (64 bits)
+  - single precision float (32 bits)
+
 ## [0.1.0] - 2020-01-27
 ### Released
 - Released library with two functions: spit-bytes and slurp-bytes.
-
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.nighcoder/binio "0.1.1-SNAPSHOT"
+(defproject org.clojars.nighcoder/binio "0.1.1"
   :description "Read and write bytes from and to file"
   :url "https://github.com/nighcoder/binio"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]]}}
-  :repl-options {:init-ns binio.core})
+  :repl-options {:init-ns org.clojars.nighcoder.binio.core})
